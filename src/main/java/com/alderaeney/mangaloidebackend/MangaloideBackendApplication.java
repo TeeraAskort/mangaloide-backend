c307c805a94539f1ee81a39d7f5e2018c131eef8
Alderaeney <alderaeney@gmail.com>
2022-03-15 21:40:30 +0100

    first commit

---
+++ src/main/java/com/alderaeney/mangaloidebackend/MangaloideBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alderaeney.mangaloidebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaloideBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaloideBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alderaeney/mangaloidebackend/MangaloideBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alderaeney.mangaloidebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaloideBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
